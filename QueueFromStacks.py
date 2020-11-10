e1a9eaade98a540bcd065742cbc8cf5f00f30948
Denys <oggyla@gmail.com>
2020-11-09 22:04:08 -0800

    Create QueueFromStacks.py

---
+++ QueueFromStacks.py
@@ -0,0 +1,30 @@
+# Create a queue using 2 stacks. A hint: stack1 will hold the contents of the actual queue, stack2 will be used in the enQueueing
+# Efficiency is not the goal!
+# Efficiency is not the goal!
+# Efficiency is not the goal!
+# The goal is to practice using one data structure to implement another one, in our case Queue from 2 Stacks
+# Queue is FIFO --> First In First out
+# Stack is LIFO --> Last  In First Out
+
+class QueueOfStacks:
+    def __init__(self):
+        self.stack1 = Stack()
+        self.stack2 = Stack()
+
+    def front(self):
+      pass
+    
+    def isEmpty(self):
+      pass
+
+    def size(self):
+      pass
+
+    def deQueue(self):
+      pass
+
+    def enQueue(self, value): 
+      pass
+    # Optional
+    def showQS(self):
+      pass
